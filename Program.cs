73d170a7512e3996ac07c44b58da22f8dd32f9f7
Zenix <33501925+0Zenix@users.noreply.github.com>
2023-09-03 17:45:46 -0300

    C# Number Base Converter (W.I.P)
    A number base converter written in C#
    (W.I.P)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,92 @@
-﻿namespace HelloWorld
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace BaseConverter
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(DecToAny("15", 16));
+        }
+
+        static int AnyToDec(String input)
+        {
+            String number = ReverseString(input.ToUpper());
+
+            int original_base = GetBase(number);
+
+            int acc = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                acc += Convert.ToInt32(Math.Pow(original_base, i)) * CharToNumber(number[i]);
+            }
+
+            return acc;
+        }
+
+        static string DecToAny(String input, int target_base)
+        {
+            int num = int.Parse(input);
+
+            int resto;
+            string results = string.Empty;
+            while (num > 0)
+            {
+                resto = num % target_base;
+                num /= target_base;
+                results = NumberToChar(resto) + results;
+            }
+
+            return new string((results).ToArray());
+        }
+
+        static int CharToNumber(char input)
+        {
+            int result = (int)input-55;
+
+            if ((int)input < 65)
+            {
+                result += 7;
+            }
+
+            return result;
+        }
+
+        static char NumberToChar(int input)
+        {
+            int result = input+48;
+
+            if (input >= 10)
+            {
+                result += 7;
+            }
+
+            return Convert.ToChar(result);
+        }
+
+        static int GetBase(String input_string)
+        {
+            int largest_yet = 0;
+
+            for (int i = 0; i < input_string.Length; i++)
+            {
+                int num = CharToNumber(input_string[i]);
+
+                if ( num > largest_yet )
+                {
+                    largest_yet = num;
+                }
+            }
+
+            return largest_yet+1;
+        }
+
+        static string ReverseString(string text)
+        {
+            return new string(text.Reverse().ToArray());
         }
     }
 }
